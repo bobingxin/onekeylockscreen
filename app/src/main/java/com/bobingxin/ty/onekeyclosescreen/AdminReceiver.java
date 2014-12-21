b38125f2e8d9021ef4ae5db9d2bf548c2fac4220
bobingxin <bobingxin@qq.com>
2014-12-21 13:03:01 +0800

    实现一键锁屏的功能，后续需要一个图标

---
+++ app/src/main/java/com/bobingxin/ty/onekeyclosescreen/AdminReceiver.java
@@ -0,0 +1,7 @@
+package com.bobingxin.ty.onekeyclosescreen;
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class AdminReceiver extends DeviceAdminReceiver {
+
+}
--- app/src/main/java/com/bobingxin/ty/onekeyclosescreen/MainActivity.java
+++ app/src/main/java/com/bobingxin/ty/onekeyclosescreen/MainActivity.java
@@ -1,5 +1,10 @@
 package com.bobingxin.ty.onekeyclosescreen;
 
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,9 +13,20 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private DevicePolicyManager mDevicePolicyManager;
+    private ComponentName mComponentName;
+    private static final int REQUEST_CODE = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        mComponentName = new ComponentName(this, AdminReceiver.class);
+        if (mDevicePolicyManager.isAdminActive(mComponentName)) {
+            mDevicePolicyManager.lockNow();
+            finish();
+        } else {
+            activeManage();
+        }
         setContentView(R.layout.activity_main);
     }
 
@@ -35,5 +51,28 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            mDevicePolicyManager.lockNow();
+            finish();
+        } else {
+            activeManage();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void activeManage() {
+        // TODO Auto-generated method stub
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);        // 权限列表
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mComponentName);
+
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"激活后您就可以一键锁屏了，让你的电源键寿命更长");
+
+
+        startActivityForResult(intent, REQUEST_CODE);
+
     }
 }
